0d8fc8d03adf56fc59fb0428e398dab59fe8af42
Ivans Sadovnikovs <78017590+ZaDivan@users.noreply.github.com>
2023-12-17 14:32:00 +0200

    Video.sql

--- video.sql
+++ video.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Окт 29 2023 г., 13:08
+-- Время создания: Дек 17 2023 г., 13:29
 -- Версия сервера: 10.4.28-MariaDB
 -- Версия PHP: 8.2.4
 
@@ -37,8 +37,10 @@ CREATE TABLE `genres` (
 --
 
 INSERT INTO `genres` (`id`, `name`) VALUES
-(1, 'Detective'),
-(2, 'Drama');
+(1, 'Detektīvs'),
+(2, 'Drāma'),
+(5, 'Dokumentāla'),
+(6, 'Trilleris');
 
 -- --------------------------------------------------------
 
@@ -52,16 +54,21 @@ CREATE TABLE `items` (
   `genre_id` int(11) NOT NULL,
   `year_id` int(11) NOT NULL,
   `description` text NOT NULL,
-  `photo` text NOT NULL
+  `photo` text NOT NULL,
+  `source_1` text NOT NULL,
+  `source_2` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Дамп данных таблицы `items`
 --
 
-INSERT INTO `items` (`id`, `name`, `genre_id`, `year_id`, `description`, `photo`) VALUES
-(1, 'The Shawshank Redemption', 2, 1, 'Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man\'s unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red.—J-S-Golden', 'https://m.media-amazon.com/images/I/61-vQDr7ecL._AC_UF894,1000_QL80_.jpg'),
-(2, 'The Girl with the Dragon Tattoo', 1, 2, 'Forty years ago, a girl from a powerful and influential family disappears under mysterious circumstances. Her uncle, convinced it was a murder case, investigates as he entrusts disgraced journalist Mikael Blomkvist (Daniel Craig) and teenage hacker Lisbeth Salander (Rooney Mara) to investigate. The film is based on bestselling Swedish novel by author Stieg Larson, which has sold over 15 million books worldwide.', 'https://www.film.ru/sites/default/files/movies/posters/1629398-812342.jpg');
+INSERT INTO `items` (`id`, `name`, `genre_id`, `year_id`, `description`, `photo`, `source_1`, `source_2`) VALUES
+(1, 'Bēgšana no Šoušenkas', 2, 1, 'Filma ir par baņķieri Endiju Defrēnu, kam piespriesti divi mūža ieslodzījumi par sievas un viņas mīļākā nogalināšanu. Cietumā viņš sadraudzējas ar Elisu Boidu \"Redu\" Redingu, Defrēns jūtas drošībā pēc tam, kad viņa zināšanas finanšu nozarē izmanto cietuma darbinieki un ieslodzītie', 'https://m.media-amazon.com/images/I/61-vQDr7ecL._AC_UF894,1000_QL80_.jpg', 'https://viedtelevizija.lv/skaties/begsana-no-sousenkas', 'https://go3.lv/movies/the-shawshank-redemption,vod-6488436'),
+(2, 'Meitene ar pūķa tetovējumu', 1, 2, 'Filma stāsta par meiteni, kas pazudusi pirms 40 gadiem un kuras liktenis nedod mieru kādam vīram. Pēdējā mēģinājumā atklāt meitenes pazušanas noslēpumu viņš noalgo žurnālistu, kas spējis atrisināt vissarežģītāko noziegumu detaļas.', 'https://www.film.ru/sites/default/files/movies/posters/1629398-812342.jpg', 'https://www.primevideo.com/dp/amzn1.dv.gti.aa4904ab-aa12-424f-bb4d-043c2cbc97f2?autoplay=0&ref_=atv_cf_strg_wb', 'https://play.google.com/store/movies/details?id=blyLV366xGg'),
+(7, 'Pēdējais no mums', 2, 3, '20 gadus pēc tam, kad modernā civilizācija ir iznīcināta, Džoels tiek nolīgts, lai aizvestu Eliju no karantīnas zonas. Šķietami vieglais darbs kļūst par brutālu ceļojumu pāri ASV, paļaujoties vienam uz otru, lai izdzīvotu.', 'https://straume.mstatic.lv/mlmt/images/video/hbo/9393/hbo_season_poster_1673837110.jpg', 'https://viedtelevizija.lv/skaties/lv-hbo-series-120814', 'https://go3.lv/series/pedejais-no-mums,serial-5404556'),
+(8, 'Pāršķirts: Attiecības digitālajā laikmetā', 5, 4, 'Atklāts skatījums uz pārmaiņām seksā un attiecībās digitālajā laikmetā, ko sniedz intervijas ar ASV jauniešiem, ekspertiem un iepazīšanās aplikāciju radītājiem.', 'https://straume.mstatic.lv/mlmt/images/video/hbo/1031939/hbo_poster_1536666738.jpg', 'https://viedtelevizija.lv/skaties/lv-hbo-swiped-hooking-up-in-the-digital-age', 'https://go3.lv/movies/parskirts-attiecibas-digitalaja-laikmeta,vod-6070874'),
+(9, 'Tenet', 6, 5, 'Bruņojies ar vienu vārdu – Tenet –, filmas galvenais varonis dodas cīņā, lai novērstu Trešo pasaules karu. Viņa misija ved cauri krēslainajiem starptautiskās spiegošanas koridoriem, un atklājas, ka tā pārkāpj esošā laika rāmjus. Tā nav ceļošana laikā. Tā ir inversija.', 'https://vtv-public.mstatic.lv/images/FILMAS_SERI%C4%80LI/Filmas/500x707/Tenet-5.jpg', 'https://viedtelevizija.lv/skaties/tenet', 'https://tet.plus/media/1608543623188');
 
 -- --------------------------------------------------------
 
@@ -79,22 +86,29 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`) VALUES
-(1, 'admin'),
-(2, 'user');
+(0, 'Admin'),
+(1, 'User');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `role_id`
+-- Структура таблицы `users`
 --
 
-CREATE TABLE `role_id` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` text NOT NULL,
   `role` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
+(27, 'kavo', '123@gmail.com', 'asd', 0);
 
 -- --------------------------------------------------------
 
@@ -113,7 +127,10 @@ CREATE TABLE `years` (
 
 INSERT INTO `years` (`id`, `name`) VALUES
 (1, '1994'),
-(2, '2011');
+(2, '2011'),
+(3, '2023'),
+(4, '2018'),
+(5, '2020');
 
 --
 -- Индексы сохранённых таблиц
@@ -140,11 +157,11 @@ ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `role_id`
+-- Индексы таблицы `users`
 --
-ALTER TABLE `role_id`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `role_id` (`role`);
+  ADD KEY `role` (`role`);
 
 --
 -- Индексы таблицы `years`
@@ -160,31 +177,31 @@ ALTER TABLE `years`
 -- AUTO_INCREMENT для таблицы `genres`
 --
 ALTER TABLE `genres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT для таблицы `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT для таблицы `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT для таблицы `role_id`
+-- AUTO_INCREMENT для таблицы `users`
 --
-ALTER TABLE `role_id`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT для таблицы `years`
 --
 ALTER TABLE `years`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -198,10 +215,10 @@ ALTER TABLE `items`
   ADD CONSTRAINT `year_id` FOREIGN KEY (`year_id`) REFERENCES `years` (`id`);
 
 --
--- Ограничения внешнего ключа таблицы `role_id`
+-- Ограничения внешнего ключа таблицы `users`
 --
-ALTER TABLE `role_id`
-  ADD CONSTRAINT `role_id` FOREIGN KEY (`role`) REFERENCES `roles` (`id`);
+ALTER TABLE `users`
+  ADD CONSTRAINT `role` FOREIGN KEY (`role`) REFERENCES `roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
